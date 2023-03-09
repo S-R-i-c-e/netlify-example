2aa983b6ee86b3e36e118da1989f137931b1a5ab
Stephen Rice <goatwipe@gmail.com>
2023-03-09 18:57:23 +0000

    stuff

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-    
+<p>
           Learn React
-
+</p>
       </header>
     </div>
   );
